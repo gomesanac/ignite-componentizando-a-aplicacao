3b76ff99922b0236ceb0f2a1d6b0917995f52548
Ana Carolina Gomes <anagomes@positivosolucoes.com.br>
2022-03-21 21:13:49 -0300

    feat: projeto componentizando a aplicação

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,35 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard';
+
+import { MovieProps } from '../@types/movies';
+import { GenreResponseProps } from '../@types/genre';
+
+interface ContentProps {
+  movies: MovieProps[];
+  selectedGenre: GenreResponseProps;
+}
+
+export function Content({ movies, selectedGenre }: ContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie: MovieProps) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
